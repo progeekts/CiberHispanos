e04f90c56b02dbf91d1ba3204c262cb10f79cf42
ProGeekts <progeekts@gmail.com>
2026-09-18 16:58:11 +0200

    Crear assets/js/app.js

---
+++ assets/js/app.js
@@ -0,0 +1,1 @@
+const b=document.querySelector('#menu'),n=document.querySelector('#nav');b?.addEventListener('click',()=>n.classList.toggle('open'));document.querySelectorAll('nav a').forEach(a=>a.addEventListener('click',()=>n.classList.remove('open')));
